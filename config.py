888dbed5c96235a3c79f8e2993ae8579b62e3e00
surya teja <121689351+surya22069@users.noreply.github.com>
2026-08-18 14:53:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
+import os
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:goli00@localhost/stockroom_db"
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "postgresql://postgres:password@localhost/stockroom_db"
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "stockroom_secret"
+    SECRET_KEY = os.getenv("SECRET_KEY", "change_this_secret")
